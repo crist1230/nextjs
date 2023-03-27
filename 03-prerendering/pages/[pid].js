4744a1c6405946f1db5f27b8c1cb8c99388dd388
Cristiana <mateas.cri@gmail.com>
2023-03-27 13:47:59 -0700

    lec 99

---
+++ 03-prerendering/pages/[pid].js
@@ -0,0 +1,31 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const ProductDetailPage = ({ loadedProduct }) => {
+    return (
+        <>
+            <h1>{loadedProduct.title}</h1>
+            <p>{loadedProduct.description}</p>
+        </>
+    );
+};
+
+export const getStaticProps = async (context) => {
+    const { params } = context;
+
+    const productId = params.pid;     
+
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    const product = data.products.find(product => product.id === productId);
+
+    return {
+        props: {
+            loadedProduct: product,
+        },
+    };
+};
+
+export default ProductDetailPage;
