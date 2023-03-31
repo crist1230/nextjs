9b4044168726a5555189c056123fd0b0532be851
Cristiana <mateas.cri@gmail.com>
2023-03-31 12:55:34 -0700

    finished section 7

--- 05-optimization/pages/events/index.js
+++ 05-optimization/pages/events/index.js
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import { getAllEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
@@ -17,6 +18,11 @@ function AllEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta name='description' content='Find great events near you...' />
+      </Head>
+
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
--- 05-optimization/pages/index.js
+++ 05-optimization/pages/index.js
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-util';
+
 import EventList from '../components/events/event-list';
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name='description' content='Find great events near you...' />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
